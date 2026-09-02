d7b5eb15894f1b5b59a9d3f7825fa2afb79ae9e0
𝔄𝔷𝔲𝔫𝔬 <237433387+chematic@users.noreply.github.com>
2026-09-03 00:22:25 +0200

    Fix fetch URL in mojavevalley.js

--- api/mojavevalley.js
+++ api/mojavevalley.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-        "https://raw.githubusercontent.com/chematic/storage/refs/heads/main/api/v1/mjvalley-v1.4"
+        "https://raw.githubusercontent.com/chematic/storage/refs/heads/main/api/v1/checker.luau"
     )
 
     const script = await response.text()
