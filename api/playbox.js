e77af7b2b95f721bf34f7ebf0ff5be9a2a10ee7b
𝔄𝔷𝔲𝔫𝔬 <237433387+chematic@users.noreply.github.com>
2026-09-19 01:21:40 +0200

    Create playbox.js

---
+++ api/playbox.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+    if (req.headers.auth !== "@vhb7") {
+        return res.status(403).send("Forbidden")
+    }
+
+    const response = await fetch(
+        "https://raw.githubusercontent.com/chematic/storage/refs/heads/main/api/v1/playboxsafari.luau"
+    )
+
+    const script = await response.text()
+
+    res.setHeader("Content-Type", "text/plain")
+    res.send(script)
+}
